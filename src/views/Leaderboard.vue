cbe46fcc94e0eddf6db4a2d412fe1ac126f0094f
Clark Baeker <clark.baeker@web.de>
2022-10-01 20:55:45 +0200

    add leaderboard place

--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -4,7 +4,7 @@ export default {
   data: function () {
     return {
       headers: [
-        { text: 'Place', value: 'index', width: '10%'},
+        { text: 'Place', value: 'place', width: '10%'},
         {
           text: 'Student',
           align: 'start',
@@ -16,26 +16,31 @@ export default {
       ],
       students: [
         {
+          place: 1,
           name: 'Rita',
           overall_correct: 86,
           questions_answered: 25,
         },
         {
+          place: 1 ,
           name: 'Mike',
           overall_correct: 76,
           questions_answered: 35,
         },
         {
+          place: 1,
           name: 'Clark',
           overall_correct: 32,
           questions_answered: 50,
         },
         {
+          place: 1,
           name: 'Yana',
           overall_correct: 91,
           questions_answered: 23,
         },
         {
+          place: 1,
           name: 'Suzanne',
           overall_correct: 83,
           questions_answered: 42,
